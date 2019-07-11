fa1a7dd4b6e5bdb51d434b67eba740a6f0e3a03f
Morse579 <yushan579@gmail.com>
2019-07-11 10:08:45 -0700

    Update part-1.R

--- part1/part-1.R
+++ part1/part-1.R
@@ -58,11 +58,7 @@ if (interactive()) {
   
   server <- function(input, output) {
     output$contents <- renderTable({
-      # input$file1 will be NULL initially. After the user selects
-      # and uploads a file, it will be a data frame with 'name',
-      # 'size', 'type', and 'datapath' columns. The 'datapath'
-      # column will contain the local filenames where the data can
-      # be found.
+      #import file
       inFile <- input$file1
       
       if (is.null(inFile))
