789110e10432148cee3c166720118de9298b4160
Morse579 <yushan579@gmail.com>
2019-07-11 21:58:30 -0700

    Update part-1.R
    build sections of the program and display the whole table

--- part1/part-1.R
+++ part1/part-1.R
@@ -22,13 +22,15 @@ library(shiny)
 #well_select<-scan()
 
 #calculation (mean and SEM)
+myparameters <- list()
+for (i in LETTERS[1:8]){
+  for(j in c("1-3","4-6","7-9","10-12")){
+    print(paste(i,j))
+    myparameters <- append(myparameters,paste(i,j))
+  }
+}
 
 #shiny
-#ui <- fluidPage("Ben Lab")
-#server <- function(input,output){}
-#shinyApp(ui=ui,server=server)
-
-## Only run examples in interactive R sessions
 if (interactive()) {
   
   ui <- fluidPage(titlePanel("Ben Lab"),
@@ -40,16 +42,21 @@ if (interactive()) {
                     "text/csv",
                     "text/comma-separated-values,text/plain",
                     ".csv"),
-
-        ),
-        tags$hr(),
-        checkboxInput("header", "Header", TRUE)
+                  ),
+        #selector
+        selectInput("wells", "Choose a well:",
+                    list('#well'=as.list(myparameters)),multiple = TRUE),
+        selectInput("fields", "Choose a field:",
+                    list('#field'=seq(1,3)),multiple = TRUE),
+        checkboxGroupInput("calculation", 
+                    label = "Calculation",choices = c("Mean","SEM"))
+        
       ),
       mainPanel(
         #tabset for plot, summary and table
         tabsetPanel(
           tabPanel("Plot", plotOutput("plot")), 
-          tabPanel("Table", tableOutput("table")),
+          tabPanel("Table", dataTableOutput("table")),
           tabPanel("Summary", verbatimTextOutput("summary")) 
         )
       )
@@ -57,16 +64,17 @@ if (interactive()) {
   )
   
   server <- function(input, output) {
-    output$contents <- renderTable({
+    output$table <- renderDataTable({
       #import file
       inFile <- input$file1
       
       if (is.null(inFile))
         return(NULL)
       
-      read.csv(inFile$datapath, header = input$header)
+      read.csv(inFile$datapath, header = TRUE)
     })
   }
+  
   
   shinyApp(ui, server)
 }
