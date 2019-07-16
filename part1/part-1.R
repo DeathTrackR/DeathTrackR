0902dd9b1f85cd6e3f5705b216246564a27ebc24
clairemhl <43197349+clairemhl@users.noreply.github.com>
2019-07-16 01:29:27 -0700

    with interaction and calculation

--- part1/part-1.R
+++ part1/part-1.R
@@ -2,6 +2,7 @@
 library(plotrix) # for standard error function
 library(tidyverse) # this includes dplyr for data wranling, ggplot2 for plotting, and tidyr for reshaping data
 library(shiny)
+library(DT)
 
 #import file
 #path <- file.choose()
@@ -45,9 +46,12 @@ if (interactive()) {
                   ),
         #selector
         selectInput("wells", "Choose a well:",
-                    list('#well'=as.list(myparameters)),multiple = TRUE),
+                    "",multiple = TRUE),
+        selectInput("para","Choose parameters:",
+                    "",multiple = TRUE),
+        # uiOutput("selecters"),
         selectInput("fields", "Choose a field:",
-                    list('#field'=seq(1,3)),multiple = TRUE),
+                    NULL,multiple = TRUE),
         checkboxGroupInput("calculation", 
                     label = "Calculation",choices = c("Mean","SEM"))
         
@@ -57,27 +61,75 @@ if (interactive()) {
         tabsetPanel(
           tabPanel("Plot", plotOutput("plot"),downloadButton('Save', 'save')), 
           tabPanel("Table", dataTableOutput("table")),
-          tabPanel("Summary", verbatimTextOutput("summary")) 
+          tabPanel("Summary", verbatimTextOutput("summary"))
         )
       )
     )
   )
   
-  server <- function(input, output) {
-    output$table <- renderDataTable({
-      #import file
-      inFile <- input$file1
-      
-      if (is.null(inFile))
-        return(NULL)
-      
-      read.csv(inFile$datapath, header = TRUE)
+  server <- function(input, output,session) {
+    # set the selector choices based on wells and parameters in input dataset
+    observeEvent(input$file1,{
+      df<- read.csv(input$file1$datapath)
+      updateSelectInput(session,"wells",choices = unique(df[,1]))
+      updateSelectInput(session,"para",choices = names(df))
     })
     
-    #plot
-    output$plot <- renderPlot({
-
+    output$table <- renderDataTable({
+    #import file
+      #filter the dataset based on parameter and wells users interested in
+      table()
     })
+    table <- reactive({
+      df<-read.csv(input$file1$datapath)
+      if(is.null(input$wells)){
+        return(df)
+      }
+      else{df %>%
+        select(input$para)%>%
+        filter(ï..Well.Name %in% input$wells)}
+      })
+    
+    observeEvent(input$calculation,{
+      if(input$calculation =="Mean"){
+        df_cal <- table()%>%
+          group_by(ï..Well.Name,Time.Point)%>%
+          summarize_all(list(~mean(.)))%>%
+          as.data.frame()%>%
+          gather(key = "variable", value = "value", -ï..Well.Name, -Time.Point)
+        output$plot <- renderPlot({
+          df_cal%>%
+            ggplot() +
+            #making bars from only means
+            geom_bar(aes(x = Time.Point, y = value, fill = variable), 
+                     stat = "identity")
+        })
+      }
+      if(input$calculation =="SEM"){
+        df_cal <- table()%>%
+          group_by(ï..Well.Name,Time.Point)%>%
+          summarize_all(list(~std.error(.)))%>%
+          as.data.frame()%>%
+          gather(key = "variable", value = "value", -ï..Well.Name, -Time.Point)
+        output$plot <- renderPlot({
+          df_cal%>%
+            ggplot() +
+            #making bars from only means
+            geom_bar(aes(x = Time.Point, y = value, fill = variable), 
+                     stat = "identity")
+        })
+      }
+    })
+    #plot
+    # output$plot <- renderPlot({
+    #   df_cal%>%
+    #     ggplot() +
+    #     #making bars from only means
+    #     geom_bar(aes(x = Time.Point, y = value, fill = parameter), 
+    #              stat = "identity")
+    #     
+    # })
+    
   }
   
   
